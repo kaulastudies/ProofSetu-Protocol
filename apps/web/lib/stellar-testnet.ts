e02e152457f11c1f5d5c3a9db49fe6c568554738
kaulastudies <kaulastudies@gmail.com>
2026-06-29 15:58:14 +0530

    Add Stellar testnet contract config

---
+++ apps/web/lib/stellar-testnet.ts
@@ -0,0 +1,14 @@
+export const STELLAR_TESTNET_CONFIG = {
+  network: "testnet",
+  contractId: "CD7VTK6VVMXCBWHOGPGWEHG3KFOLNSYMV3DNGK5BDMRWH74HGKLZRGGZ",
+  wasmHash:
+    "743718b322e6dc81af3677bafadd236f4a85a76c87171a7847c582f25b400166",
+  deployerPublicAddress:
+    "GAT2L4GYPN2TST44AQA6QFVNWB4BUM7SXC73B4D5Y7PA4YRNHCLL",
+  deploymentTransaction:
+    "5ee448eb186c966fe6965fa7282b94d78a96df4e596dd9d0f6326cf3d0f6261b",
+  createProofTransaction:
+    "8a7fd049fcaf57e0c438a1b521b08cd30afe762cd6b74c172c302faccd98f132",
+  sampleProofId: "PS-TESTNET-001",
+  sampleEventHash: "sample_testnet_hash_001",
+} as const;
